3521c92103d19fc3dd01f4e10f5fbe065c678a37
Sandraj Saju <sandrajdevamangalam@gmail.com>
2024-02-09 16:45:05 +0530

    scroll

--- client/src/components/ActorChatWindow.jsx
+++ client/src/components/ActorChatWindow.jsx
@@ -65,7 +65,7 @@ const ActorChatWindow = ({ messages, setMessages, id, findParticularChat, actorI
 
       useEffect(() => {
         if (chatContainerRef.current) {
-            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight
         }
     }, [messages]);
 
